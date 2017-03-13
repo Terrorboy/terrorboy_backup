46f257ed33df7d2bc0708ab5455b77f1c8101db8
terrorboy <terrorboy@daum.net>
2017-03-13 21:30:07 +0900

    포트변경

--- 자작/PHP/TBClass/TerrorBoy.class.php
+++ 자작/PHP/TBClass/TerrorBoy.class.php
@@ -634,7 +634,7 @@ class Terrorboy {
 		public function TBConsole($Data='', $HeaderInfo='') {
 
 			$server = 'z9n.net'; // 콘솔서버 주소
-			$port = 4000; // 콘솔 포트
+			$port = 80; // 콘솔 포트
 
 			if(is_array($Data)) $Data = http_build_query($Data);
 			if($HeaderInfo == 1) {
